87dc219bcdb5a7f3b915f802b6e8ed689f463f7d
ZalmanFrz <zalmanofficial3@gmail.com>
2024-11-06 10:45:53 +0530

    Comparison Operators In JavaScript

---
+++ Comparison_Operator.js
@@ -0,0 +1,25 @@
+// Comparison Operators Example//
+
+//== Double equal to 
+console.log(3 == 3);
+
+//!= not equal to
+console.log(3 != 3);
+
+console.log(3 > 4);
+//> Greater Than 
+
+// < Less Than 
+console.log(3 < 4);
+
+// > = Greater Than or equal to
+console.log(3 >= 2);
+
+// < = Less Than or equal to
+console.log(3 <= 3);
+
+// === Strictly equal to
+console.log(3 === 3);
+
+// !== Strictly not equal to
+console.log(2 !== 3);
